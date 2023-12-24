0a1d94961a73ed29544a83374ad586247c5874cd
M777ks <m777ks@gmail.com>
2023-12-24 17:10:04 +0400

    2day

---
+++ handlers/other_hendlers.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+# Этот хэндлер будет реагировать на любые сообщения пользователя,
+# не предусмотренные логикой работы бота
+@router.message()
+async def send_echo(message: Message):
+    await message.answer(f'Это эхо! {message.text} \n Топай в /help ')
